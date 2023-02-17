76618a238a8c6ce84f51db0282b8d2eedbf8ba1a
Mwabisi <pmwabisiradebe@gmail.com>
2023-02-17 08:33:02 -0800

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome of art,");
+	return(0);
