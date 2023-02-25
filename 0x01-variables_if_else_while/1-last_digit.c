5f88da118aa06c9fd4d53c5c44aec89331136e68
Mwabisi <pmwabisiradebe@gmail.com>
2023-02-25 07:36:05 -0800

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *
+ * main - start point
+ *
+ * Program assigns a random number to variable n to see if its positive or not
+ *
+ * Return:0
+ */
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
