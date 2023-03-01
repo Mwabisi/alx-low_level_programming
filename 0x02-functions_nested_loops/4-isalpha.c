eba9bb97f7537ea486d7a0a326a1f5a18d0e73be
Mwabisi <pmwabisiradebe@gmail.com>
2023-02-28 23:58:55 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,16 @@
 #include"main.h"
 
 /**
- @c: the charactetr to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - Checks for alphabetic character
+ * @c : The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
+
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'Z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
