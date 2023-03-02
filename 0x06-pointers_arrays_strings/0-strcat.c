68545a163af7c37f67f22764b4343c30a35d23c5
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-02 06:03:20 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,13 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[dlen])
 	{
-		dlen++
+		dlen++;
 	}
 
 	for (i = 0; src[i] != 0; i++)
 	{
 		dest[dlen] = src[i];
-		dlen++
+		dlen++;
 	}
 	dest[dlen] = '\0';
 	return (dest);
