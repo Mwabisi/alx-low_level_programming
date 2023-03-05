524b5f180a2bb03143ce20e03abed93a202b6a76
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-05 04:06:52 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return: 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multiligual puzzle");
-
-	return(0);
-}
-	
+	return (0);
+}	
