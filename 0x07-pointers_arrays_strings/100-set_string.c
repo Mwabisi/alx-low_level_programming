dbadfc2a4b6df848f5f5a1b61ec47ead3c6cea69
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-07 05:30:21 -0800

    All tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - sets the first string to the second string
- * @s: string to be changed
- * @to: string s is to be changed to
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
+
 void set_string(char **s, char *to)
 {
-	*s = &*to;
+	*s = to;
 }
-
