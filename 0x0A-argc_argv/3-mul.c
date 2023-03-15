c420ffeb17ebad97375add0ba9903b575e7b9917
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-15 08:15:52 -0700

    Neither irony nor sarcasm is argument mandatory

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints the result of the multiplication of 2 numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int num1, num2;
+
+	num2 = 1;
+	if (argc < 3)
+	{
+
+		printf("Error\n");
+		return (1);
+	}
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		num2 = num2 * atoi(argv[num1]);
+	}
+	printf("%d\n", num2);
+	return (0);
+}
