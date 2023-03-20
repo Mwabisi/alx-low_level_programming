2a5f2624d680d347f8c463622a3427051cbef254
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-20 14:34:02 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - Allocates memory
+ * @b: The size to allocate
+ * Return: Always 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+0x0C. C - More malloc, free
+
+the project is about molloc, rellloc and using tbe exit function
