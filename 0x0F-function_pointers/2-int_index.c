efb2a708fb8da2eb29e1063024104a549e0af39b
Mwabisi <pmwabisiradebe@gmail.com>
2023-03-29 01:09:47 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
- * @size: size of elements in array
+ * @size: size of element in array
  * @cmp: pointer to func of one of the 3 in main
  * Return: 0
  */
